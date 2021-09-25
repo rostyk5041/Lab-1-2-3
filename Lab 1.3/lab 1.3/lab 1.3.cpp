b364d71afaa4f392f46b89bce09adeb3d6cdb53d
Your Name <you@example.com>
2021-09-25 17:58:05 +0300

    first master commit

---
+++ Lab 1.3/lab 1.3/lab 1.3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello, World!" << endl;
+	int p = 1;
+	int i = 1;
+	while (i <= 10)
+	{
+		p *= i;
+		i++;
+	};
+	cout << "p = " << p << " i = " << i << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ Lab 3.3/Lab 3.3/Lab 3.3.cpp
@@ -0,0 +1,34 @@
+﻿// Lab_03_3.cpp
+// Іватьо Р.М. 
+// Лабораторна робота № 3.3
+// Варіант 6
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x<=-5)
+		y = -3;
+	else
+		if (-5 < x && x <= -R)
+			y = (3*(R+x))/(5-R);
+		else
+			if (-R < x && x <= R)
+				y = sqrt(R*R- x * x);
+			else
+				if (R < x && x <= 8)
+					y = (R*x - R*R )/(8 - R);
+				else
+					y = R;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
